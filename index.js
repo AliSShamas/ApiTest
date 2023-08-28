b7c4d9f265be4881c37828572483b872f6eb76ce
Ali Shamas <127535679+zZSlinkerZz@users.noreply.github.com>
2023-08-28 20:38:50 +0300

    l

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
+var cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+app.use(cors());
 
 app.get('/',(_req,res)=>{
   res.send("Hi All");
 });
 app.get('/data',(req,res)=>{
-  var data = ['Ali','Geogrio','Rym','Sara']
+  var data = ['Ali','Geogrio','Rym','Sarah','Steve','Alex','Josh','Chris','Nour','Samir','Britteny','Yaldez','Hazar']
   res.send(data);
 });
 
